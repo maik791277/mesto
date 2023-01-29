2b151fe43d3eea004071fd7052027bff4f7679b4
Vlad Potylitsyn <vlad_maik7912@vk.com>
2023-01-29 10:49:25 +0200

    Практическая работа 5 - правка

--- script/index.js
+++ script/index.js
@@ -32,7 +32,7 @@ const initialCards = [
 ];
 
 // Находим в DOM
-const profileForm  = document.querySelector('.profile')
+const profileForm = document.querySelector('.profile')
 const bodyPreload = document.querySelector('.body')
 //Находим popup
 const popupUserTitle = document.querySelector('.popup_add_user-title');
@@ -41,16 +41,14 @@ const popupUserImage = document.querySelector('.popup-image')
 //Находим кнопки открытия popup
 const addButtonCard = profileForm.querySelector('.profile__add-button')
 const addButtonTitle = profileForm.querySelector('.profile__edit-button');
-//Находим кнопки закрытия popup
-const closeButtonTitle = popupUserTitle.querySelector('.popup__close-button');
-const closeButtonCard = popupUserCard.querySelector('.popup__close-button');
-const closeButtonImage = popupUserImage.querySelector('.popup__close-button')
+// находим все крестики проекта по универсальному селектору
+const closeButtons = document.querySelectorAll('.popup__close-button');
 // Находим поля формы Card
 const nameInputCard = popupUserCard.querySelector('.popup__input_field_name');
 const jobInputCard = popupUserCard.querySelector('.popup__input_field_job')
 // Находим поля формы Image
 const popupImageSrc = document.querySelector('.popup-image__image');
-const popupUserImageTitle =document.querySelector('.popup-image__title');
+const popupUserImageTitle = document.querySelector('.popup-image__title');
 // Находим поля формы Title
 const nameInputTitle = popupUserTitle.querySelector('.popup__input_field_name');
 const jobInputTitle = popupUserTitle.querySelector('.popup__input_field_job')
@@ -62,17 +60,21 @@ const cardTemplate = document.querySelector('#place-card').content;
 const cardGrid = document.querySelector('.card-grid__cards');
 
 
-
-
 // функции открытия закрытия
 function openPopup(popupElement) {
     popupElement.classList.add('popup_opened')
 }
+
 function closePopup(popupElement) {
     popupElement.classList.remove('popup_opened')
 }
 
+// устанавливаем обработчик закрытия на крестик
+closeButtons.forEach((button) => {
+    const popup = button.closest('.popup');
 
+    button.addEventListener('click', () => closePopup(popup));
+});
 
 
 //форма Template
@@ -81,54 +83,49 @@ function addCard(name, link, alt,) {
     const cardElementImage = cardElement.querySelector('.card__image');
     const cardElementTitle = cardElement.querySelector('.card__title');
     const removeCards = cardElement.querySelector('.card__remove');
-    cardElement.querySelector('.card__image-button').addEventListener('click',function () {
+    cardElement.querySelector('.card__image-button').addEventListener('click', function () {
         openPopup(popupUserImage)
         popupImageSrc.src = `${link}`;
         popupImageSrc.alt = `${alt}`;
         popupUserImageTitle.textContent = `${name}`;
-    }); closeButtonImage.addEventListener('click', function () {
-        closePopup(popupUserImage)
-    })
+    });
     cardElementImage.src = `${link}`;
     cardElementImage.alt = `${alt}`;
     cardElementTitle.textContent = `${name}`;
     removeCards.addEventListener('click', function () {
         cardElement.remove();
-    }); cardElement.querySelector('.card__like').addEventListener('click', function (evt) {
-        evt.target.classList.toggle('card__like_active')
     });
+    cardElement.querySelector('.card__like')
+        .addEventListener('click', function (evt) {
+            evt.target.classList.toggle('card__like_active')
+        });
+
     return cardElement
 }
+
 // перебор маисва
 initialCards.forEach(function asd(el) {
     cardGrid.append(addCard(el.name, el.link, el.alt))
 })
 
 
-
-
-
 //форма Card
 // Кнопка открытия формы Card
 addButtonCard.addEventListener('click', () => openPopup(popupUserCard));
-// Кнопка закрытия формы Card
-closeButtonCard.addEventListener('click', () => closePopup(popupUserCard));
+
 // Обработчик «отправки» формы.Card
 function addNewCard(evt) {
     evt.preventDefault();
-    let nameInput = nameInputCard.value;
-    let linkInput = jobInputCard.value;
-    let altInput = nameInputCard.value;
+    const nameInput = nameInputCard.value;
+    const linkInput = jobInputCard.value;
+    const altInput = nameInputCard.value;
     cardGrid.prepend(addCard(nameInput, linkInput, altInput));
     closePopup(popupUserCard)
     evt.target.reset()
 }
+
 // он будет следить за событием “submit” - «отправка»
 popupUserCard.addEventListener('submit', addNewCard);
-
-
-
-
 
 
 //форма Title
@@ -138,8 +135,7 @@ addButtonTitle.addEventListener('click', function () {
     nameInputTitle.value = nameProfile.textContent;
     jobInputTitle.value = jobProfile.textContent;
 });
-// Кнопка закрытия формы Title
-closeButtonTitle.addEventListener('click', () => closePopup(popupUserTitle));
+
 // Обработчик «отправки» формы.
 function handleProfileFormSubmit(evt) {
     evt.preventDefault();
@@ -148,11 +144,9 @@ function handleProfileFormSubmit(evt) {
 
     closePopup(popupUserTitle);
 }
+
 // он будет следить за событием “submit” - «отправка»
 popupUserTitle.addEventListener('submit', handleProfileFormSubmit);
-
-
-
 
 
 //Используем слушителя и этам загрузки load для того чтобы,
